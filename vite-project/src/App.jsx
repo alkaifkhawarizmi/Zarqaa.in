03f2ad42f53ab3247339ae3c52f7008b50935db4
Alkaif <alkaifkhawarizmi@gmail.com>
2024-11-17 09:26:59 +0530

    start again

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,14 +1,9 @@
-import Center from "./Center"
-import Header from "./Header"
-import TopOfferDiv from "./Top"
 
 function App() {
 
   return (
-    <div className="overflow-hidden mb-10 bg-[#FFF6F2]">
-      <TopOfferDiv />
-      <Header />
-      <Center />
+    <div className="">
+      
     </div>
   )
 }
